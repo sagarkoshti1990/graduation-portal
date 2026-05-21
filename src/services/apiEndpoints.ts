542f24e94e38d805553c5f405eaeb5d9826441a3
sagar <sagarkoshti1990@gmila.com>
2026-05-22 02:06:44 +0530

    fix:observation form defult value by createdFromQuestionId + add prefix to categories API

--- src/services/apiEndpoints.ts
+++ src/services/apiEndpoints.ts
@@ -16,7 +16,7 @@ export const API_ENDPOINTS = {
   PARTICIPANTS_LIST: `${prefix}/project/v1/programUsers/entities`,
   PARTICIPANTS_SUB_ENTITY_LIST: `${prefix}/entity-management/v1/entities/subEntityList`,
   ENTITY_DETAILS: `${prefix}/entity-management/v1/entities/details`,
-  PROJECT_CATEGORIES_LIST: `/project/v1/library/categories/list?parentId=null&keywords=idp&getChildren=true`,
+  PROJECT_CATEGORIES_LIST: `${prefix}/project/v1/library/categories/list?parentId=null&keywords=idp&getChildren=true`,
   GET_ENTITY_DETAILS: (id: string) =>
     `${prefix}/entity-management/v1/entities/details/${id}`,
   UPDATE_ENTITY_DETAILS: `${prefix}/project/v1/programUsers/createOrUpdate`,
