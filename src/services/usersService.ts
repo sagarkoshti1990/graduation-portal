b429a1e708da560e717949ef7850157e47ae4cf6
rohitkodamtekdi <rohitkodamtekdi@gmail.com>
2026-08-19 11:32:51 +0530

    Resolved PR Comments

--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -25,7 +25,7 @@ import logger from '@utils/logger';
 export const getUsersList = async (params: UserSearchParams): Promise<UserSearchResponse> => {
   try {
     const {
-      tenant_code = process.env.TENANT_CODE_NAME || 'brac',
+      tenant_code = process?.env?.TENANT_CODE_NAME || 'brac',
       type = ROLE_NAMES.USER,
       page = 1,
       limit = 20,
